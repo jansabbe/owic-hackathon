7ec41fdf97d33e4d7fd53fecaabca2383c469717
Jan Sabbe <jan.sabbe@gmail.com>
2016-09-14 17:06:31 +0200

    Added ngfor and separate component

--- Ui/src/bootstrap.ts
+++ Ui/src/bootstrap.ts
@@ -3,10 +3,11 @@ import {AppComponent} from './app/app.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {GalleryComponent} from './app/gallery/gallery.component';
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent, GalleryComponent],
     bootstrap: [AppComponent]
 })
 class AppModule{}
