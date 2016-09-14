97129e3d205f251431f50017ed059fa32ad82481
Bart Wijnants <Bart Wijnants>
2016-09-14 16:52:45 +0200

    petcupids!!!!

---
+++ PetCupid.cs
@@ -0,0 +1,12 @@
+namespace PetCupid
+{
+    public class PetCupid
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Kind { get; set; }
+        public string Img { get; set; }
+        public string ProfileText { get; set; }
+        public int Popularity { get; set; }
+    }
+}
